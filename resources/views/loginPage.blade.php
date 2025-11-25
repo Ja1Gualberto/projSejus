2c136b04fa8c69708c0fed0220700a038efb2a83
Joao <gualberto.silvaj@gmail.com>
2025-11-25 13:30:34 -0400

    add loginPage remake navbar style

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -1,13 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="resources/css/bootstrap.css">
+    <link rel="stylesheet" href="{{ asset('css/styleLogin.css') }}?v={{ file_exists(public_path('css/styleLogin.css')) ? filemtime(public_path('css/styleLogin.css')) : time() }}">
     <title>Login</title>
 </head>
-<body>
 
+<body>
+    <section class="login-box">
+        <a href="{{Route('homePage')}}"><img src="{{asset('images/logoNova.png')}}" alt="" width="50px" height="50px" ></a>
+        <h2>Login</h2>
+        <div class="form-box">
+            <form action="">
+                <div for="inp" class="email-box">
+                    <label for="email">
+                       <input type="email" name="email" id="email" placeholder="&nbsp;">
+                       <span class="label">Email</span>
+                       <span class="focus-bg"></span>
+                    </label>
+                </div>
+                <div class="password-box">
+
+                    <label for="password">
+                      <input type="password" name="password" id="password" placeholder="&nbsp;">
+                      <span class="label">Senha</span>
+                      <span class="focus-bg"></span>
+                    </label>
+                </div>
+                <button class="submit-btn">Login</button>
+                <p>Ainda não tem conta? <a href="{{Route('registerPage')}}">Cadastre-se</a></p>
+            </form>
+        </div>
+    </section>
 </body>
+
 </html>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,22 +4,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Layout</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('css/styleNavbar.css')}}?v={{ file_exists(public_path('css/styleNavbar.css')) ? filemtime(public_path('css/styleNavbar.css')) : time() }}">
   </head>
   <body>
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand">Sejus Play</a>
-    <a href="{{Route('homePage')}}">Home</a>
-    <a href="{{Route('gamesPage')}}">Catalogo</a>
-    <a href="{{Route('aboutUs')}}">Sobre Nós</a>
-    <a href="{{Route('myProfile')}}"><i class="fa-solid fa-circle-user"></i></a>
-    <form class="d-flex" role="search">
-      {{-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> --}}
-        <a href="{{ route('login') }}" class="btn btn-outline-success">Login</a>
-    </form>
-  </div>
+<nav class="teste">
+  <div class="nav-container">
+        <a class="logo" href="{{Route('homePage')}}"><img src="{{asset('images/logoNova.png')}}" alt="" width="60px" height="60px" ></a>
+
+        <div class="nav-links">
+            <a class="link-navbar" href="{{Route('homePage')}}">Home</a>
+            <a class="link-navbar" href="{{Route('gamesPage')}}">Catalogo</a>
+            <a class="link-navbar" href="{{Route('aboutUs')}}">Sobre Nós</a>
+            <a class="link-navbar" href="{{Route('myProfile')}}"><i class=""></i></a>
+        </div>
+        <a class="login" href="{{ route('login') }}" class="">Login</a>
+</div>
 </nav>
 <main>
     @yield('content')
